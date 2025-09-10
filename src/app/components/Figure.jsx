a16dcb316efc502b2e64f81a93b4c1c0a6756ce7
arunchakrawarti <arunmojampur@gmail.com>
2025-09-10 17:12:56 +0530

    puch

--- src/app/components/Figure.jsx
+++ src/app/components/Figure.jsx
@@ -6,53 +6,53 @@ const Figure = () => {
       <img
         src="https://algorooms.com/svgs/icon-card-1.svg"
         alt=""
-        className="w-12 sm:w-16 md:w-20 lg:w-24"
+        className="w-12 transform-origin:center hover:scale-125 origin-center transition-transform duration-500 sm:w-16 md:w-20 lg:w-24"
       />
       <img
         src="https://algorooms.com/svgs/icon-card-2.svg"
         alt=""
-        className="w-12 sm:w-16 md:w-20 lg:w-24"
+        className="w-12 transform-origin:center hover:scale-125 origin-center transition-transform duration-500 sm:w-16 md:w-20 lg:w-24"
       />
       <img
         src="https://algorooms.com/svgs/icon-card-3.svg"
         alt=""
-        className="w-12 sm:w-16 md:w-20 lg:w-24"
+        className="w-12 transform-origin:center hover:scale-125 origin-center transition-transform duration-500 sm:w-16 md:w-20 lg:w-24"
       />
       <img
         src="https://algorooms.com/svgs/icon-card.svg"
         alt=""
-        className="w-12 sm:w-16 md:w-20 lg:w-24"
+        className="w-12 transform-origin:center hover:scale-125 origin-center transition-transform duration-500 sm:w-16 md:w-20 lg:w-24"
       />
       <img
         src="https://algorooms.com/svgs/icon-card-10.svg"
         alt=""
-        className="w-12 sm:w-16 md:w-20 lg:w-24"
+        className="w-12 transform-origin:center hover:scale-125 origin-center transition-transform duration-500 sm:w-16 md:w-20 lg:w-24"
       />
       <img
         src="https://algorooms.com/svgs/icon-card-11.svg"
         alt=""
-        className="w-12 sm:w-16 md:w-20 lg:w-24"
+        className="w-12 transform-origin:center hover:scale-125 origin-center transition-transform duration-500 sm:w-16 md:w-20 lg:w-24"
       />
       <img
         src="https://algorooms.com/svgs/icon-card-4.svg"
         alt=""
-        className="w-12 sm:w-16 md:w-20 lg:w-24"
+        className="w-12 transform-origin:center hover:scale-125 origin-center transition-transform duration-500 sm:w-16 md:w-20 lg:w-24"
       />
       <img
         src="https://algorooms.com/svgs/icon-card-7.svg"
         alt=""
-        className="w-12 sm:w-16 md:w-20 lg:w-24"
+        className="w-12 transform-origin:center hover:scale-125 origin-center transition-transform duration-500 sm:w-16 md:w-20 lg:w-24"
       />
       <img
         src="https://algorooms.com/svgs/icon-card-8.svg"
         alt=""
-        className="w-12 sm:w-16 md:w-20 lg:w-24"
+        className="w-12 transform-origin:center hover:scale-125 origin-center transition-transform duration-500 sm:w-16 md:w-20 lg:w-24"
       />
       <div className="basis-full"></div>
       <img
         src="https://algorooms.com/svgs/icon-card-9.svg"
         alt=""
-        className="w-12 sm:w-16 md:w-20 lg:w-24"
+        className="w-12 transform-origin:center hover:scale-125 origin-center transition-transform duration-500 sm:w-16 md:w-20 lg:w-24"
       />
     </div>
   );
